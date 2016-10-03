c11b153de0981a751f210bd85c8a9b1b597afecf
spyke42 <arturian42@gmail.com>
2016-10-03 06:32:51 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
 import math
 def reversing(str1):
 	S = str1.split(' ')
-	S = S.reverse()
-	return S
+	Name = S[0].reverse()
+	SName = S[1].reverse()
+	F = Name+SName
+	return F
 r='Hello World'
 result = reversing(r)
 print(result)
