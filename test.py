71d6c0f7f1264c74265337bea159bbe0911902bb
spyke42 <arturian42@gmail.com>
2016-10-04 11:19:37 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,2 @@
-import math
-def reversing(str1):
-	S = str1.split(' ')
-	Name = reversed(S[0])
-	SName = reversed(S[1])
-	F = Name+SName
-	return F
-r='Hello World'
-result = reversing(r)
-print(result)
+number = input("Put the number" )
+print(numbers)
